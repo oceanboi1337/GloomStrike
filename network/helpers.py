cadb48072e3acb5c4bb67cd9f117b7d60f9ecae9
satan <28350180+oceanboi1337@users.noreply.github.com>
2023-05-23 02:39:42 +0200

    Auto fetch src address

--- network/helpers.py
+++ network/helpers.py
@@ -1,95 +1,97 @@
-import ipaddress, socket, struct, random, re, os
+import ipaddress, socket, struct, random, re, os, netifaces, json
 
 def nic(dst : ipaddress._IPAddressBase) -> ipaddress._IPAddressBase:
 
-    family = socket.AF_INET if dst.version == 4 else socket.AF_INET6
-    #network = ipaddress.ip_network(dst)
-    interfaces = socket.getaddrinfo(socket.gethostname(), None, family, 1, 0)
+	default_gateway = socket.getaddrinfo('_gateway', 0)[0][4][0]
 
-    return ipaddress.ip_address(interfaces[0][4][0])
+	gateways = netifaces.gateways()
 
-    """for family, sock_type, protocol, flags, addr in interfaces:
+	for gateway in [v for k, v in gateways.items() if type(v) == dict]:
+			
+		addr, iface = gateway.get(socket.AF_INET)
 
-        src = ipaddress.ip_address(addr[0])
-    """
+		if addr == default_gateway:
+			
+			for addresses in netifaces.ifaddresses(iface).get(socket.AF_INET):
+				
+				return addresses['addr']
 
+	
 def is_valid_domain(domain : str):
 
-    r = re.compile('^([a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,}$')
-    return r.match(domain)
+	r = re.compile('^([a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,}$')
+	return r.match(domain)
 
 def is_valid_host(host : str):
 
-    try:
-        return ipaddress.ip_address(host)
-    except:
-        if is_valid_domain(host):
-            try:
-                addr = socket.getaddrinfo(host, 0)
-                return is_valid_host(addr[0][4][0])
-            except Exception as e:
-                print(f'[ERROR]: Failed to resolve {host}', e)
-    
+	try:
+		return ipaddress.ip_address(host)
+	except:
+		if is_valid_domain(host):
+			try:
+				addr = socket.getaddrinfo(host, 0)
+				return is_valid_host(addr[0][4][0])
+			except Exception as e:
+				print(f'[ERROR]: Failed to resolve {host}', e)
+	
 def is_valid_network(network : str):
 
-    try:
-        return ipaddress.ip_network(network)
-    except:
-        pass
+	try:
+		return ipaddress.ip_network(network)
+	except:
+		pass
 
 def calculate_checksum(packet):
 
-    if len(packet) % 2 == 1:
-        packet += b'\0'
+	if len(packet) % 2 == 1:
+		packet += b'\0'
 
-    data = struct.unpack('!%dH' % (len(packet) // 2), packet)
-    
-    checksum = sum(data)
-    checksum += (checksum & 0xffff >> 16)
-    checksum += (checksum >> 16)
+	data = struct.unpack('!%dH' % (len(packet) // 2), packet)
+	
+	checksum = sum(data)
+	checksum += (checksum & 0xffff >> 16)
+	checksum += (checksum >> 16)
 
-    return ~checksum & 0xffff
+	return ~checksum & 0xffff
 
-def create_packet_syn(src : ipaddress._BaseAddress, dst : ipaddress._BaseAddress, port : int):
+def create_packet_syn(src_addr : ipaddress._BaseAddress, dst_addr : ipaddress._BaseAddress, src_port : int, dst_port : int):
 
-    src_port = 1337
-    dst_port = port
-    seq = 0
-    ack = 0
-    flags = 0x2
-    length = 0x28
-    window = 5840
-    checksum = 0
-    pointer = 0
+	seq = 0
+	ack = 0
+	flags = 0x2
+	length = 0x28
+	window = 5840
+	checksum = 0
+	pointer = 0
 
-    tmp_ip_header = struct.pack('!BBHHHBBH4s4s',
-                            (src.version << 4) + 5, 0,
-                            length, os.getpid() & 0xffff,
-                            0, 255,
-                            socket.IPPROTO_TCP, checksum,
-                            src.packed, dst.packed)
-    
-    ip_header = struct.pack('!BBHHHBBH4s4s',
-                            (src.version << 4) + 5, 0,
-                            0x28, os.getpid() & 0xffff,
-                            0, 255,
-                            socket.IPPROTO_TCP, calculate_checksum(tmp_ip_header),
-                            src.packed, dst.packed)
+	tmp_ip_header = struct.pack('!BBHHHBBH4s4s',
+							(src_addr.version << 4) + 5, 0,
+							length, os.getpid() & 0xffff,
+							0, 255,
+							socket.IPPROTO_TCP, checksum,
+							src_addr.packed, dst_addr.packed)
+	
+	ip_header = struct.pack('!BBHHHBBH4s4s',
+							(src_addr.version << 4) + 5, 0,
+							0x28, os.getpid() & 0xffff,
+							0, 255,
+							socket.IPPROTO_TCP, calculate_checksum(tmp_ip_header),
+							src_addr.packed, dst_addr.packed)
 
-    tmp_tcp_header = struct.pack('!HHLLBBHHH',
-                             src_port, dst_port,
-                             seq, ack, (5 << 4) + 0,
-                             flags, window,
-                             checksum, pointer)
+	tmp_tcp_header = struct.pack('!HHLLBBHHH',
+							 src_port, dst_port,
+							 seq, ack, (5 << 4) + 0,
+							 flags, window,
+							 checksum, pointer)
 
-    pseudo_header = struct.pack('!4s4sBBH', src.packed, dst.packed, 0, socket.IPPROTO_TCP, len(tmp_tcp_header))
-    checksum = calculate_checksum(pseudo_header + tmp_tcp_header)
+	pseudo_header = struct.pack('!4s4sBBH', src_addr.packed, dst_addr.packed, 0, socket.IPPROTO_TCP, len(tmp_tcp_header))
+	checksum = calculate_checksum(pseudo_header + tmp_tcp_header)
 
-    tcp_header = struct.pack('!HHLLBBHHH',
-                             src_port, dst_port,
-                             seq, ack, (5 << 4) + 0,
-                             flags, window,
-                             checksum, pointer)
+	tcp_header = struct.pack('!HHLLBBHHH',
+							 src_port, dst_port,
+							 seq, ack, (5 << 4) + 0,
+							 flags, window,
+							 checksum, pointer)
 
-    return ip_header + tcp_header
-    
+	return ip_header + tcp_header
+	
