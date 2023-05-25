8557f850cd499e76e841c53945aa6f110c011841
unknown <28350180+oceanboi1337@users.noreply.github.com>
2023-05-25 11:16:50 +0200

    Added background threading

--- hashcrack/hashcrack.py
+++ hashcrack/hashcrack.py
@@ -1,7 +1,7 @@
 import mmap, hashlib, threading, time, multiprocessing, helpers, os
 from logger import Logger
 
-def _worker(path, results, hashes, start, end):
+def _worker(algorithm, path, results, hashes, start, end):
 
     with open(path, 'r+b') as f:
 
@@ -22,7 +22,7 @@ def _worker(path, results, hashes, start, end):
 
             word = word.rstrip()
 
-            m = hashlib.new('md5')
+            m = hashlib.new(algorithm)
             m.update(word)
             word_hash = m.hexdigest()
 
@@ -46,8 +46,8 @@ class Hashcrack:
         self.db = db
         self.logger = logger
 
-        self.processors = os.cpu_count() - 1
-        self.processes : list(multiprocessing.Process) = []
+        self._processors = os.cpu_count() - 1
+        self._processes : list(multiprocessing.Process) = []
 
         self.manager = multiprocessing.Manager()
         self._results = self.manager.dict()
@@ -55,6 +55,10 @@ class Hashcrack:
 
         self._wordlist_size = 0
         self._wordlist_path = None
+
+    @property
+    def status(self):
+        return len(self._processes)
 
     def load_wordlist(self, wordlist : str):
 
@@ -88,37 +92,15 @@ class Hashcrack:
             #self._hashes = self.manager.list(hashes)
             self._hashes = hashes
 
+            return True
+
         except Exception as e:
             self.logger.error(f'Failed to load hashes: {e}')
 
-    def crack(self):
-
-        self.logger.info(f'Logical CPUs: {self.processors + 1}')
-        self.logger.info(f'Using {self.processors}')
-
-        if self._wordlist_size <= 0 and len(self._hashes) > 0:
-            return False
-
-        increment = int(self._wordlist_size / self.processors)
-
-        start = 0
-        end = int(increment)
-
-        start_time = time.time()
-
-        for pid in range(self.processors):
-
-            proc = multiprocessing.Process(target=_worker, args=[self._wordlist_path, self._results, self._hashes, start, end])
-            proc.start()
-
-            start = int(end) - 1024
-            end += int(increment)
-
-            self.processes.append(proc)
-
-            self.logger.info(f'Started {proc.name}')
+    def _crack(self, algorithm : str):
 
         log_list = []
+        start_time = time.time()
 
         while 1:
 
@@ -130,7 +112,7 @@ class Hashcrack:
                 self.logger.info(f'Cracked Hash in {round(time.time() - start_time, 2)} sec {hash} -> {word}')
                 log_list.append(hash)
 
-            if len([proc for proc in self.processes if proc.is_alive()]) == 0:
+            if len([proc for proc in self._processes if proc.is_alive()]) == 0:
                 break
 
             if len(self._results) == len(self._hashes):
@@ -142,13 +124,51 @@ class Hashcrack:
 
         self._results = dict(self._results)
 
-        for proc in self.processes:
-
-            proc.join()
-            self.processes.remove(proc)
+        for proc in [proc for proc in self._processes]:
+            proc.kill()
+            self._processes.remove(proc)
 
         self.manager.shutdown()
 
         return self._results
 
+    def start(self, algorithm : str, background : bool=False):
+
+        if self._wordlist_size <= 0 and len(self._hashes) > 0:
+            return False
         
+        if algorithm not in hashlib.algorithms_available:
+            
+            self.logger.error(f'Hashing algorithm {algorithm} is not available')
+            return False
+
+        self.logger.info(f'Logical CPUs: {self._processors + 1}')
+        self.logger.info(f'Using {self._processors}')
+
+        increment = int(self._wordlist_size / self._processors)
+
+        start = 0
+        end = int(increment)
+
+        for pid in range(self._processors):
+
+            proc = multiprocessing.Process(target=_worker, args=[algorithm, self._wordlist_path, self._results, self._hashes, start, end])
+            proc.start()
+
+            start = int(end) - 1024
+            end += int(increment)
+
+            self._processes.append(proc)
+
+            self.logger.info(f'Started {proc.name}')
+
+        if background:
+
+            self.background_thread = threading.Thread(target=self._crack, args=[algorithm])
+            self.background_thread.setDaemon = True
+            self.background_thread.start()
+
+            return True
+        
+        else:
+            return self._crack(algorithm)
